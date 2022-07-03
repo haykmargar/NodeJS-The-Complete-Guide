e1a261e50e9d2751dbcbb3f72830656a1e4d8b85
Hayk_Margaryan1 <hayk_margaryan1@epam.com>
2022-07-03 16:19:01 +0400

    implemented dynamic templating system with EJS

--- NodeJS-The-Complete-Guide/app.js
+++ NodeJS-The-Complete-Guide/app.js
@@ -1,6 +1,26 @@
-const http = require('http');
-const routes = require('./routes')
+const path = require('path');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const rootDir = require('./util/path');
 
-server.listen(3000);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
