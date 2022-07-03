a529d2a57b4e7125f278d29d6271199c1c86f388
Hayk_Margaryan1 <hayk_margaryan1@epam.com>
2022-07-03 17:24:02 +0400

    implemented data storing in a json file

--- NodeJS-The-Complete-Guide/models/product.js
+++ NodeJS-The-Complete-Guide/models/product.js
@@ -1,15 +1,41 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path')
+
+const p = path.join(
+    rootDir,
+    'data',
+    'products.json');
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            return callback([]);
+        } else {
+            callback(JSON.parse(data));
+        }
+    });
+}
+
 
 module.exports = class Product {
     constructor(title) {
-        this.title = title
+        this.title = title;
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+        fs.readFile(p, (err, data) => {
+        })
     };
 
-    static fetchAll(){
-        return products;
-    }
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    };
 };
