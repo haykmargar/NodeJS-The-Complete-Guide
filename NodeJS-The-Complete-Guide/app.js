d23e9b4b5761090642a2ed543763617fbabea95f
Hayk_Margaryan1 <hayk_margaryan1@epam.com>
2022-07-03 16:51:42 +0400

    saving products in an array

--- NodeJS-The-Complete-Guide/app.js
+++ NodeJS-The-Complete-Guide/app.js
@@ -3,23 +3,23 @@ const express = require('express');
 
 const rootDir = require('./util/path');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const errorController = require('./controllers/error')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(express.urlencoded());
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
